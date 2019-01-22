4674351d494081e9c771cc8bf81ba03809448deb
lpiaitdepok <lpiaitdepok@gmail.com>
2019-01-22 13:05:44 +0700

    Create row_number_ritel.sql

---
+++ SQLite/row_number_ritel.sql
@@ -0,0 +1,12 @@
+SELECT
+    ROW_NUMBER () OVER ( 
+        ORDER BY NamaBarang 
+    ) RowNum,
+    NamaBarang,
+    Satuan,
+    HargaBeli,
+    HargaJual,
+    Diskon,
+    Stok     
+FROM
+    Barang;
